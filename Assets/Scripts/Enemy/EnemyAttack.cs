9f8ad2866cc0f7ab798912cb90d46c8b25848b11
Muhammad Malik Ibrahim Baharsyah <13521029@mahasiswa.itb.ac.id>
2024-05-06 12:04:55 +0700

    feat: merge

--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -9,38 +9,50 @@ public class EnemyAttack : MonoBehaviour
 
     Animator anim;
     GameObject player;
+    GameObject pet;
     PlayerHealth playerHealth;
+    PetHealth petHealth;
     EnemyHealth enemyHealth;
     bool playerInRange;
+    bool petInRange;
     float timer;
 
 
-    void Awake ()
+    void Awake()
     {
-        player = GameObject.FindGameObjectWithTag ("Player");
-        playerHealth = player.GetComponent <PlayerHealth> ();
-        anim = GetComponent <Animator> ();
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+        anim = GetComponent<Animator>();
         // Mendapatkan Enemy health
         enemyHealth = GetComponent<EnemyHealth>();
     }
 
     // Callback jika ada suatu object masuk ke dalam trigger
-    void OnTriggerEnter (Collider other)
+    void OnTriggerEnter(Collider other)
     {
         // Set player in range
         if (other.gameObject == player && other.isTrigger == false)
         {
             playerInRange = true;
-            
+
+        }
+        // Set pet in range
+        if (other.gameObject == pet && other.isTrigger == false)
+        {
+            petInRange = true;
         }
     }
 
     // Callback jika ada object yang keluar dari trigger
     void OnTriggerExit(Collider other)
     {
-        if(other.gameObject == player && other.isTrigger == false)
+        if (other.gameObject == player && other.isTrigger == false)
         {
             playerInRange = false;
+        }
+        if (other.gameObject == pet && other.isTrigger == false)
+        {
+            petInRange = false;
         }
     }
 
@@ -49,9 +61,20 @@ public class EnemyAttack : MonoBehaviour
     {
         timer += Time.deltaTime;
 
-        if(timer >= timeBetweenAttacks && playerInRange && enemyHealth.currentHealth > 0)
+        pet = GameObject.FindGameObjectWithTag("PlayerPet");
+        if (pet != null)
+        {
+            petHealth = pet.GetComponent<PetHealth>();
+        }
+
+        if (timer >= timeBetweenAttacks && playerInRange && enemyHealth.currentHealth > 0)
         {
             Attack();
+        }
+
+        if (timer >= timeBetweenAttacks && petInRange && enemyHealth.currentHealth > 0)
+        {
+            AttackPet();
         }
 
         if (playerHealth.currentHealth <= 0)
@@ -69,6 +92,17 @@ public class EnemyAttack : MonoBehaviour
         if (playerHealth.currentHealth > 0)
         {
             playerHealth.TakeDamage(attackDamage);
+        }
+    }
+
+    void AttackPet()
+    {
+        timer = 0f;
+
+        // Taking damage
+        if (petHealth.currentHealth > 0)
+        {
+            petHealth.TakeDamage(attackDamage);
         }
     }
 }
---
+++ Assets/Scripts/Pet/PetHealth.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetHealth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int startingHealth = 100;
+    public int currentHealth;
+    public float sinkSpeed = 2.5f;
+
+    CapsuleCollider capsuleCollider;
+    bool isDead;
+    bool isSinking;
+
+    void Awake()
+    {
+        capsuleCollider = GetComponent<CapsuleCollider>();
+        currentHealth = startingHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isSinking)
+        {
+            transform.Translate(-Vector3.up * sinkSpeed * Time.deltaTime);
+        }
+    }
+
+    void Death()
+    {
+        isDead = true;
+        capsuleCollider.isTrigger = true;
+        StartSinking();
+    }
+
+    public void StartSinking()
+    {
+        GetComponent<UnityEngine.AI.NavMeshAgent>().enabled = false;
+        GetComponent<Rigidbody>().isKinematic = true;
+        isSinking = true;
+        Destroy(gameObject, 2f);
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (isDead)
+            return;
+
+        currentHealth -= amount;
+
+        if (currentHealth <= 0)
+        {
+            Death();
+        }
+    }
+}
