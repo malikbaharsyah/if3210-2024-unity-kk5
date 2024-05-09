6cd3a5b47482e285be7ef337b463fed2a58d7a40
Muhammad Malik Ibrahim Baharsyah <13521029@mahasiswa.itb.ac.id>
2024-05-09 21:51:03 +0700

    feat: shopkeeper

--- Assets/Scripts/Managers/ScoreManager.cs
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -1,24 +1,35 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
     public static int score;
 
 
-    Text text;
+    TextMeshProUGUI text;
 
 
     void Awake ()
     {
-        text = GetComponent<Text>();
+        text = GetComponent<TextMeshProUGUI>();
         score = 0;
     }
 
 
     void Update ()
     {
-        text.text = "Score: " + score;
+        text.text = score.ToString();
+    }
+
+    public int getScore()
+    {
+        return score;
+    }
+
+    public void setScore(int inputScore)
+    {
+        score = inputScore;
     }
 }
